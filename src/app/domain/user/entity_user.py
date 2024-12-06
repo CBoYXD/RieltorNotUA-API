1e14dc7420f809809aa2c22bad685e39d9de753f
CBoYXD <python.rust.cpp@gmail.com>
2024-12-06 15:13:08 +0200

    Delete methods from entity

--- src/app/domain/user/entity_user.py
+++ src/app/domain/user/entity_user.py
@@ -15,26 +15,3 @@ class User(Entity[Id]):
     full_name: UserFullname
     role: UserRole
     is_active: bool
-
-    @classmethod
-    def create(
-        cls, *, user_id: UUID, email: str, 
-        full_name: str, role: UserRole, username: str
-    ) -> User:
-        return cls(
-            id_=Id(user_id),
-            username=UserName(username),
-            email=UserEmail(email),
-            full_name=UserFullname(full_name),
-            role=role,
-            is_active=True,
-        )
-
-    def activate(self) -> None:
-        self.is_active = True
-
-    def inactivate(self) -> None:
-        self.is_active = False
-
-    def grant_role(self, role: UserRole) -> None:
-        self.role = role
--- src/app/domain/user_auth/entity_user_auth.py
+++ src/app/domain/user_auth/entity_user_auth.py
@@ -2,14 +2,9 @@ from dataclasses import dataclass
 from uuid import UUID
 
 from app.domain.user.entity_user import User
-from app.domain.user.vo_user import UserId, UserEmail, UserFullname
 from app.domain.user_auth.vo_user_auth import UserPasswordHash
 
 
 @dataclass(eq=False, kw_only=True)
 class UserAuth(User):
     password: UserPasswordHash
-
-    @classmethod
-    def create(cls, password: str, **kwargs) -> UserAuth:
-        return cls(password=password, **super().create(**kwargs))
