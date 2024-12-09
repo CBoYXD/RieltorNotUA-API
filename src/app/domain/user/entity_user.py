2fe42a978fb7f548c5187d906dce9aee843d9528
CBoYXD <python.rust.cpp@gmail.com>
2024-12-09 23:32:21 +0200

    Add relationship to user entity

--- src/app/domain/user/entity_user.py
+++ src/app/domain/user/entity_user.py
@@ -3,6 +3,7 @@ from typing import Optional
 from uuid import UUID
 
 from app.domain.base.entity import Entity
+from app.domain.offer.entity_offer import Offer
 from app.domain.base.value_object import Id
 from app.domain.user.enums import UserRole
 from app.domain.user.vo_user import UserFullname, UserEmail, UserName
@@ -15,3 +16,4 @@ class User(Entity[Id]):
     full_name: UserFullname
     role: UserRole
     is_active: bool
+    offers: list[Offer] = []
