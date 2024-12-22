59683478dd7b4683b53335962affb958fe91f480
CBoYXD <python.rust.cpp@gmail.com>
2024-12-22 23:07:39 +0200

    Add UUIDGenerator

--- src/app/domain/user/entity_user.py
+++ src/app/domain/user/entity_user.py
@@ -3,6 +3,7 @@ from typing import List
 from dataclasses import dataclass, field
 
 from src.app.domain.base.entity import Entity
+from src.app.domain.base.ports.uuid_generator import UUIDGenerator
 from src.app.domain.base.value_object import Id
 from src.app.domain.offer.entity_offer import Offer
 from src.app.domain.user.enums import UserRole
@@ -16,4 +17,17 @@ class User(Entity[Id]):
     full_name: UserFullname
     role: UserRole
     is_active: bool
-
+    
+    
+    def create(
+        id_generator: UUIDGenerator, email: str, username: str,
+        full_name: str, role: UserRole, is_active: bool
+    ) -> User:
+        return User(
+            id_=Id(UUIDGenerator()),
+            email=UserEmail(email),
+            username=UserName(username),
+            full_name=UserFullname(full_name),
+            role=role,
+            is_active=is_active
+        )
