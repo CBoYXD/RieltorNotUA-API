c45f89881e5d0fb3a208b64f2649837baae99c56
CBoYXD <python.rust.cpp@gmail.com>
2025-01-01 18:04:52 +0200

    Add auth usecases

---
+++ src/app/application/cabinet/ports/repo.py
@@ -0,0 +1,34 @@
+from abc import abstractmethod
+from typing import Protocol, List
+
+from src.app.domain.base.value_object import Id
+from src.app.domain.offer.entity_offer import Offer
+from src.app.domain.offer_details.entity_offer_details import OfferDetails
+
+
+class CabinetRepository(Protocol):
+    @abstractmethod
+    async def get_all_offers(self, user_id: Id) -> list[Offer]:
+        """
+        :raises DataMapperError:
+        """
+        
+    @abstractmethod
+    async def get_all_full_offers(self, user_id: Id) -> list[OfferDetails]:
+        """
+        :raises DataMapperError:
+        """
+
+    @abstractmethod
+    async def get_offer_by_id(self, user_id: Id, offer_id: Id) -> Offer | None:
+        """
+        :raises DataMapperError:
+        """
+
+    @abstractmethod
+    async def get_full_offer_by_id(self, user_id: Id, offer_id: Id) -> OfferDetails | None:
+        """
+        :raises DataMapperError:
+        """
+        
+    # TODO: Add changing settings
